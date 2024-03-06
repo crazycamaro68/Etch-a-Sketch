5e11ca1e4283130aa8c210849fbf99b63fc0195a
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-03-05 21:39:05 -0500

    add code to create grid based off number. changed the css to format it correctly.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
-alert("test");
+let gridBox = document.querySelector('#gridBox');
+let gridSize = 6;
+
+
+
+
+const genGrid = function(gridSize){
+for(i = 0; i < gridSize; i++){
+    let boxRow = document.createElement('div');
+    boxRow.setAttribute('id', 'boxRow'+i);
+    boxRow.setAttribute('class', 'boxFlex');
+    gridBox.appendChild(boxRow);
+}
+for(int = 0; int < gridSize; int++){
+    let boxRow = document.querySelector('#boxRow'+int);
+    rowCreate(boxRow, gridSize);
+}
+}
+const rowCreate = function(boxRow, gridSize){
+    for(i = 0; i < gridSize; i++){
+    let boxCol = document.createElement('div');
+    boxCol.setAttribute('id', "boxCol"+ i);
+    boxCol.setAttribute('class', 'boxCol')
+    boxRow.appendChild(boxCol);
+    }
+}
+genGrid(6);
+const colorChange = function(){
+const hoverBox = document.getElementsByClassName(".boxCol");
+
+
+hoverBox.addEventListener("mouseover", hoverChangeColor(), false);
+function hoverChangeColor(){
+    alert('test');
+}; 
+}
+colorChange();
+
+
